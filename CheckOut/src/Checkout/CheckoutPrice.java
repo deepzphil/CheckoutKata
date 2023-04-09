11ff67481b939df9e526c5f3ca6303fdff72372b
deepzphil <deepujessphilip@gmail.com>
2023-04-09 14:00:02 +0100

    Changes for Code Refactoring

--- CheckOut/src/Checkout/CheckoutPrice.java
+++ CheckOut/src/Checkout/CheckoutPrice.java
@@ -5,81 +5,87 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class CheckoutPrice {
-	 public static Map<Character, Integer> itemPrices = new HashMap<>();
-	  public static  Map<Character, Offer> offerItems = new HashMap<>();
-	  static {
-itemPrices.put('A',50);
-itemPrices.put('A', 50);
-itemPrices.put('B', 30);
-itemPrices.put('C', 20);
-itemPrices.put('D', 15);
+	 public static Map<String, Integer> itemPrices = new HashMap<>();
+	 public static Map<String, Offer> offerItems = new HashMap<>();
 
-  offerItems.put('A', new Offer(3, 130));
-  offerItems.put('B', new Offer(2, 45));
+	 static {
+		itemPrices.put("A", 50);
+		itemPrices.put("B", 30);
+		itemPrices.put("C", 20);
+		itemPrices.put("D", 15);
+
+		offerItems.put("A", new Offer(3, 130));
+		offerItems.put("B", new Offer(2, 45));
 	  }
+	 
 	  public static void main(String[] args) {
+		  Scanner scanner = new Scanner(System.in);
+
 		  try {
-	        @SuppressWarnings("resource")
-			Scanner scanner = new Scanner(System.in);
-	        System.out.println("Enter the item");
-	        Map<Character, Integer> cart = new HashMap<>();
+	        System.out.println("Enter the item name:");
+	        Map<String, Integer> cart = new HashMap<>();
 
 	        while (true) {
-	            String input = scanner.nextLine();
-	            if (input.equalsIgnoreCase("done")) {
+	            String item = scanner.nextLine();
+
+	            if (item.equalsIgnoreCase("done")) {
 	                break;
 	            }
-
-	            char item = input.charAt(0);
+	            
 	            if (!itemPrices.containsKey(item)) {
 	                System.out.println("Invalid item! Please try again.");
 	                continue;
 	            }
 
-	            // add item to cart
+	            // Adds item to cart
 	            cart.put(item, cart.getOrDefault(item, 0) + 1);
 	            int total = calculateTotal(cart);
 	            System.out.println("Current total: " + total + " pence.");
 	        }
 
-	        int total = calculateTotal(cart);
-	        System.out.println("Final total: " + total + " pence. Thank you for shopping with us!");
-	    }catch(Exception ex) {
-			ex.printStackTrace();  
-		  } 
+	        int finalPrice = calculateTotal(cart);
+	        System.out.println("Final total: " + finalPrice + " pence. Thank you for shopping with us!");
+		  } catch(Exception ex) {
+			  // TODO: add error logging to text file			  
+	          System.out.println("There was an error during the process. Exception details: " + ex.getMessage());
+		  } finally {
+			  scanner.close();
+		  }
 	  }
-
-	    static int calculateTotal(Map<Character, Integer> cart) {
-	        int total = 0;
-
-	        for (Map.Entry<Character, Integer> entry : cart.entrySet()) {
-	            char item = entry.getKey();
-	            int count = entry.getValue();
-	            int price = itemPrices.get(item);
-	            Offer specialOffer = offerItems.get(item);
-
-	            if (specialOffer != null && count >= specialOffer.quantity) {
-	                int specialPriceCount = count / specialOffer.quantity;
-	                int regularPriceCount = count % specialOffer.quantity;
-	                total += specialPriceCount * specialOffer.price + regularPriceCount * price;
-	            } else {
-	                total += count * price;
-	            }
-	        }
-
-	        return total;
-	    }
-
 	  
-	  public static class Offer {
-	        int quantity;
-	        int price;
-
-	        public Offer(int quantity, int price) {
-	            this.quantity = quantity;
-	            this.price = price;
+	  static int calculateTotal(Map<String, Integer> cart) {
+	    int total = 0;
+	
+	    for (Map.Entry<String, Integer> entry : cart.entrySet()) {
+	        String item = entry.getKey();
+	        int cartIndex = entry.getValue();
+	        int price = itemPrices.get(item);
+	        
+	        Offer specialOffer = offerItems.get(item);
+	        
+	        if (specialOffer != null && cartIndex >= specialOffer.quantity) {
+	            int specialPriceCount = cartIndex / specialOffer.quantity;
+	            int regularPriceCount = cartIndex % specialOffer.quantity;
+	            
+	            total += (specialPriceCount * specialOffer.price) + (regularPriceCount * price);
+	            System.out.println("A special price has been given for item " + item);
+	        } else {
+	            total += cartIndex * price;
 	        }
-	  }
+	    }
+	
+	    return total;
+	}
+	
+	public static class Offer {
+		int quantity;
+		int price;
+
+        public Offer(int quantity, int price) {
+            this.quantity = quantity;
+            this.price = price;
+        }
+	}
 }
 	        
 
